f3a138eb239946c6301f5d994ff3fd9dcb3dc1fb
Ricardolv <richardluizv@gmail.com>
2020-10-19 18:17:20 -0300

    configurando projeto

---
+++ src/main/java/br/com/richard/domain/bitcoin/Bitcoin.java
@@ -0,0 +1,13 @@
+package br.com.richard.domain.bitcoin;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Bitcoin {
+
+    private Long id;
+    private BigDecimal preco;
+    private String tipo;
+    private LocalDate data;
+
+}
